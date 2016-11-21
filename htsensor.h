d27de9e6216caf9f78a097336d5fbb06b636dbd7
Ayke van Laethem <aykevanlaethem@gmail.com>
2016-11-21 16:54:53 +0100

    Send sensor values each 60s to the central server

--- htsensor.h
+++ htsensor.h
@@ -1,8 +1,10 @@
 
+#include <ArduinoJson.h>
+
 #include "DHT.h"
 
 const uint8_t HT_PIN = D6;
-const uint32_t HT_INTERVAL = 5000; // 5s
+const uint32_t HT_INTERVAL = 60000; // 1 minute
 DHT _dht(HT_PIN, DHT22);
 
 class HTSensor {
@@ -17,10 +19,29 @@ class HTSensor {
 
     void loop() {
       uint32_t currentMillis = millis();
-      if (currentMillis - lastMillis > HT_INTERVAL) {
+      if (lastMillis == 0 || currentMillis - lastMillis > HT_INTERVAL) {
         lastMillis = currentMillis;
         temperature = _dht.readTemperature(false, false);
         humidity = _dht.readHumidity(false);
+
+        yield();
+
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject& root = jsonBuffer.createObject();
+        root["time"] = Clock.timestamp();
+        root["interval"] = (HT_INTERVAL / 1000);
+        const size_t msgMaxLen = 60; // measured 46 bytes
+        char msg[msgMaxLen];
+        if (!isnan(temperature)) {
+          root.set("value", temperature, 1);
+          const size_t msgLen = root.printTo(msg, msgMaxLen);
+          mqtt.publish(MQTT_PREFIX "s/temperature", (uint8_t*)msg, msgLen, 1);
+        }
+        if (!isnan(humidity)) {
+          root.set("value", humidity, 1);
+          const size_t msgLen = root.printTo(msg, msgMaxLen);
+          mqtt.publish(MQTT_PREFIX "s/humidity", (uint8_t*)msg, msgLen, 1);
+        }
       }
     }
 
